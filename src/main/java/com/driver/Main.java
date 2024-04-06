5783bcf30f91e8c211a00cedba0c1d671dcd2e37
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-06 13:04:18 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    RWOnly obj = new RWOnly();
+        // Task 4: Error - The member 'name' is private and cannot be accessed directly
+        // System.out.println(obj.name); 
+        // Task 6: Error - The member 'name' is private and cannot be accessed directly
+        // obj.name = "John"; 
+        // System.out.println(obj.name); 
+        // These lines would generate errors because 'name' is a private member.
+        
+        // Task 6: Using setter and getter
+        obj.setName("John");
+        System.out.println(obj.getName());
 }
